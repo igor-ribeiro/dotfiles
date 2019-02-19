f5431fa86f927108315db5f759738dbeb8e06d1a
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2019-02-19 10:21:21 -0300

    Add vim plugin dependencies

--- setup.sh
+++ setup.sh
@@ -2,15 +2,21 @@
 
 # --
 # BASH
+# --
 ln -sf $(pwd)/.bashrc $HOME/
 
 # --
 # VIM
+# --
 mkdir -p $HOME/.vim
+
+# Dependencies
+npm i -g import-js import-sort-cli import-sort-parser-babylon import-sort-parser-typescript
 
 ln -sf $(pwd)/vim/autoload $HOME/.vim/
 ln -sf $(pwd)/.vimrc $HOME/
 
 # --
 # TMUX
+# --
 ln -sf $(pwd)/.tmux.conf $HOME/
