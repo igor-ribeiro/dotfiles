ff12a48ad0e835c828de840a6e1f311c5890d925
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2019-02-19 10:22:40 -0300

    Install vimx

--- setup.sh
+++ setup.sh
@@ -13,8 +13,12 @@ mkdir -p $HOME/.vim
 # Dependencies
 npm i -g import-js import-sort-cli import-sort-parser-babylon import-sort-parser-typescript
 
+# Link files
 ln -sf $(pwd)/vim/autoload $HOME/.vim/
 ln -sf $(pwd)/.vimrc $HOME/
+
+# Install vimx to enable system clipboard support
+sudo dnf install vim-X11 -y
 
 # --
 # TMUX
