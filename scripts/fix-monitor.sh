8d005102b19f7a3cb17d4288d2c3bcfaac3df2ef
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2023-05-31 10:31:13 -0300

    update

--- scripts/fix-monitor.sh
+++ scripts/fix-monitor.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-if [[ $(xrandr | rg 'HDMI-0 connected') ]]; then
-  xrandr --output HDMI-0 --auto --primary
-  xrandr --output eDP-1-1 --left-of HDMI-0 --auto
+hdmi_output=$(xrandr | rg 'HDMI-0 connected')
+
+if [[ $hdmi_output ]]; then
+  hdmi=$(echo $hdmi_output | cut -d ' ' -f1)
+  xrandr --output $hdmi --auto --primary
+  xrandr --output eDP-1-1 --left-of $hdmi --auto
 else
   xrandr --output eDP-1-1 --auto --primary
 fi
